7e0e4f79c2872302f22f54c8317ba4fb897cec74
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-14 10:26:50 -0300

    j

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -322,7 +322,7 @@ App.aplicacoes = (function ()
         mensagem,
         "#0fc",
         "Bold 16px Trebuchet MS",
-        objCanvas.canvasWidth/2 + objCanvas.canvasWidth/3 + ((BASE/22)) - 25,
+        objCanvas.canvasWidth/2 + objCanvas.canvasWidth/5 + ((BASE/22)) - 25,
         380
       ]);
 

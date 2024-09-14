daeead1d640a0a37532dc6742bec90ad6479e514
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-14 19:01:51 -0300

    bb

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -655,8 +655,8 @@ App.aplicacoes = (function ()
     "Fat",
     "#0fc",
     "Bold 14px Trebuchet MS",
-    pontoHA[0] + 10,
-    pontoHA[1] + 10
+    pontoHA[0] + 2,
+    pontoHA[1] + 2
   ]);
 
   }

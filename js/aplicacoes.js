4c220f36afc1ae9ff5a1c72a5194e2f4d57ed984
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-15 19:55:31 -0300

    hhh

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -819,7 +819,7 @@ App.aplicacoes = (function ()
     ]);
 
     // Solicita a próxima animação somente enquanto o bloco estiver no limite do plano inclinado
-    var parar = pontoA1[1] >= Y_ZERO + 50 || pontoA1[0] >= X_ZERO + 50;
+    var parar = pontoA1[1] >= Y_ZERO + 60 || pontoA1[0] >= X_ZERO + 60;
 
     if(parar){ // TODO Fazer a validação correta aqui 
      document.getElementById("voltar").disabled = false;

04ef4e211d2ec70131123187f573038f523db466
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-15 12:53:10 -0300

    nn

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -486,7 +486,7 @@ App.aplicacoes = (function ()
     var Fat = Math.min(Fatd, px); // modificação Canato
     BASE_Py = (objCanvas.canvasWidth)/(reduzdimensao/py);
     BASE_Px = (objCanvas.canvasWidth)/(reduzdimensao/px);
-    BASE_Fat = (objCanvas.canvasWidth)/(reduzdimensao*Fat); // modificação Canato
+    BASE_Fat = (objCanvas.canvasWidth)/(reduzdimensao); // modificação Canato
 
       
     //limpeza inicial da tela, para reconstrução

81c2dff57699f68d3844cad854e1b2659f0775d9
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-15 08:50:15 -0300

    dd

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -78,7 +78,7 @@ App.aplicacoes = (function ()
     aceleracaoAtualParaAnimacao = null;
     BASE_Py = null;
     BASE_Px = null;
-    BASE_Fatd = null;
+    // BASE_Fatd = null; modificação Canato
     initialTime = null;
     currentTime = null;
 

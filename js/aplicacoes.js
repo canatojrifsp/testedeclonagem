554a086b8637714c3618adce80258b7cab783438
Osvaldo Canato Jr <canatojr@ifsp.edu.br>
2024-09-16 07:40:25 -0300

    jj

--- js/aplicacoes.js
+++ js/aplicacoes.js
@@ -488,9 +488,9 @@ App.aplicacoes = (function ()
 
     BASE_Py = (objCanvas.canvasWidth)/(reduzdimensao/py);
     BASE_Px = (objCanvas.canvasWidth)/(reduzdimensao/px);
-    if(μd >= μe){(BASE_Fat = objCanvas.canvasWidth)/(reduzdimensao/Fatd);}else{(BASE_Fat = objCanvas.canvasWidth)/(reduzdimensao/px);}
+    //if(μd >= μe){(BASE_Fat = objCanvas.canvasWidth)/(reduzdimensao/Fat);}else{(BASE_Fat = objCanvas.canvasWidth)/(reduzdimensao/px);}
     //BASE_Fat = Math.max((objCanvas.canvasWidth)/(reduzdimensao/Fatd), (objCanvas.canvasWidth)/(reduzdimensao/px)); // modificação Canato
-
+    BASE_Fat = (objCanvas.canvasWidth)/(reduzdimensao/Fat);
       
     //limpeza inicial da tela, para reconstrução
     //somente o canvas superior
